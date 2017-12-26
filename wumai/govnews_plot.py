d5f23510f2ae5886bfe778a0fd64cc7055ced5b0
hf <hfloveyy@icloud.com>
2017-12-26 22:32:11 +0800

    plotly

--- wumai/govnews_plot.py
+++ wumai/govnews_plot.py
@@ -5,9 +5,9 @@ import plotly.graph_objs as go
 words = ['发展','中国','经济','企业','合作','改革','国家','创新','增长','建设','工作','推进',
 '李克强','总理','服务','推动','我国','投资','加强','问题']
 
-line = [x for x in range(100,10,10)]
+line = [x for x in range(10)]
 
-for x in range(10,100,10):
+for x in line:
     print(x)
-print(line)
+print(line[:-7:-2])
 
--- wumai/wumai.py
+++ wumai/wumai.py
@@ -2,7 +2,7 @@ from time import time
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
-from sklearn.datasets import fetch_20newsgroups
+#from sklearn.datasets import fetch_20newsgroups
 
 n_samples = 2000
 n_features = 1000
@@ -12,13 +12,22 @@ n_topics = 1
 n_top_words = 20
 cut = []
 
+score = []
+topics = []
+
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
-        print(topic.argsort())
         print("Topic #%d:" % topic_idx)
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
-    print()
+        for i in topic.argsort()[:-n_top_words - 1:-1]:
+            score.append(str(topic[i]))
+            topics.append(feature_names[i])
+            #print("score:" + str(topic[i]))
+            #print("topic:"+ feature_names[i])
+            #print("\n")
+
+
+        #print(" ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
+
 
 def load_stop_words(path = 'stop_words.txt'):
     with open(path,'r',encoding="gbk") as f:
@@ -27,7 +36,7 @@ def load_stop_words(path = 'stop_words.txt'):
             stop_words.append(content)
 
 def load_data(path = 'cut.txt'):
-    with open(path,'r') as f:
+    with open(path,'r',encoding="gbk") as f:
         cut = f.readlines()
     return cut
 # Load the 20 newsgroups dataset and vectorize it. We use a few heuristics
@@ -58,6 +67,7 @@ t0 = time()
 tf = tf_vectorizer.fit_transform(data_samples)
 print("done in %0.3fs." % (time() - t0))
 
+'''
 # Fit the NMF model
 print("Fitting the NMF model with tf-idf features,"
       "n_samples=%d and n_features=%d..."
@@ -77,7 +87,7 @@ print("Fitting LDA models with tf features, n_samples=%d and n_features=%d..."
       % (n_samples, n_features))
 
 
-
+'''
 #LDA模型
 lda = LatentDirichletAllocation(n_components=n_topics, max_iter=5,
                                 learning_method='online', learning_offset=50.,
@@ -88,4 +98,28 @@ print("done in %0.3fs." % (time() - t0))
 
 print("\nTopics in LDA model:")
 tf_feature_names = tf_vectorizer.get_feature_names()
+
+
+
 print_top_words(lda, tf_feature_names, n_top_words)
+
+
+print(topics)
+
+import plotly.offline as py
+import plotly.graph_objs as go
+
+
+
+data = [go.Bar(
+            x = topics,
+            #x=['发展','中国','经济','企业','合作','改革','国家','创新','增长','建设','工作','推进','李克强','总理','服务','推动','我国','投资','加强','问题'],
+            y = score
+    )]
+layout = go.Layout(
+    xaxis=dict(tickangle=-45),
+    barmode='group',
+)
+fig = go.Figure(data=data, layout=layout)
+py.plot(fig, filename='govnews.html')
+
